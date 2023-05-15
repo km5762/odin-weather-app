b6db923e527f3ab24104f4ef858f401950157c31
km5762 <km5762@DESKTOP-587P2TU>
2023-05-15 12:23:52 -0400

    Final commit

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+const search = document.querySelector(".search svg");
+const searchBar = document.querySelector(".search input");
+
+search.addEventListener("click", () => {
+  renderWeatherAt(searchBar.value);
+});
+
 async function getWeatherAt(location) {
   const url =
     "https://api.weatherapi.com/v1/current.json?key=e137765b2150444d9f332519231305&q=";
@@ -7,5 +14,31 @@ async function getWeatherAt(location) {
   } else {
     const json = await response.json();
     return json;
+  }
+}
+
+async function renderWeatherAt(location) {
+  const city = document.querySelector(".city");
+  const weatherCard = document.querySelector(".weather-card");
+  try {
+    const weather = await getWeatherAt(location);
+    const condition = document.querySelector(".condition");
+    const temperature = document.querySelector(".temperature");
+    const feelsLike = document.querySelector(".feels-like");
+    const wind = document.querySelector(".wind");
+    const humidity = document.querySelector(".humidity");
+    const percipitation = document.querySelector(".percipitation");
+
+    weatherCard.style.display = "flex";
+    city.textContent = weather.location.name;
+    condition.textContent = weather.current.condition.text;
+    temperature.textContent = `Temperature: ${weather.current.temp_f}`;
+    feelsLike.textContent = `Feels like: ${weather.current.feelslike_f}`;
+    wind.textContent = `Wind speed: ${weather.current.wind_mph}`;
+    humidity.textContent = `Humidity ${weather.current.humidity}`;
+    percipitation.textContent = `Precipitation: ${weather.current.precip_in}`;
+  } catch {
+    weatherCard.style.display = "flex";
+    city.textContent = `City "${location}" not found`;
   }
 }
